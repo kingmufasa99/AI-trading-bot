4ab55e622822bf2668dd9e79bda0a4344958a7eb
Moulay-Mostafa Filali <moulay-mostafa.filali.1@ulaval.ca>
2025-02-03 22:20:01 -0500

    feat: order class init

---
+++ order.py
@@ -0,0 +1,6 @@
+class Order:
+    """Classe définissant les types d'ordres disponibles."""
+    BUY = "BUY"
+    SELL = "SELL"
+    SHORT = "SHORT"
+    # Vous pouvez ajouter d'autres types d'ordres si nécessaire.
